5dda1826ae6207a14670aac0d6c4fb9b2c10ecf6
saim888 <saimjutt9900@gmail.com>
2025-08-21 16:18:27 +0500

    04:18pm

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -12,32 +12,32 @@ export default function Hero() {
             <img
                 src="/img/1.png"
                 alt="Decoration"
-                className="absolute top-[-6vh] left-[-16vw] w-[30vw] max-w-[470px] xl:w-[470px]"
+                className="absolute top-[-6vh] left-[-16vw] w-[30vw] max-w-[470px] xl:w-[470px] xl:max-w-[470px]"
             />
             <img
                 src="/img/2.png"
                 alt="Decoration"
-                className="absolute top-[-27vh] right-[-5vw] w-[30vw] max-w-[410px] xl:w-[410px]"
+                className="absolute top-[-27vh] right-[-5vw] w-[30vw] max-w-[410px] xl:w-[410px] xl:max-w-[410px]"
             />
             <img
                 src="/img/center.png"
                 alt="Decoration"
-                className="absolute top-[-51vh] left-1/2 -translate-x-1/2 w-[40vw] max-w-[500px] xl:w-[500px]"
+                className="absolute top-[-51vh] left-1/2 -translate-x-1/2 w-[40vw] max-w-[500px] xl:w-[500px] xl:max-w-[500px]"
             />
             <img
                 src="/img/bottom-right.png"
                 alt="Bottom Right"
-                className="absolute bottom-[-20vh] right-[-20vw] w-[40vw] max-w-[500px] xl:w-[500px] z-0"
+                className="absolute bottom-[-20vh] right-[-20vw] w-[40vw] max-w-[500px] xl:w-[500px] xl:max-w-[500px] z-0"
             />
             <img
                 src="/img/4.png"
                 alt="Bottom Right Corner"
-                className="absolute bottom-0 right-[-5vw] w-[15vw] max-w-[220px] xl:w-[220px] z-20"
+                className="absolute bottom-0 right-[-5vw] w-[15vw] max-w-[220px] xl:w-[220px] xl:max-w-[220px] z-20"
             />
             <img
                 src="/img/5.png"
                 alt="5"
-                className="absolute bottom-[23vh] right-[2vw] w-[15vw] max-w-[200px] xl:w-[200px] z-10"
+                className="absolute bottom-[23vh] right-[2vw] w-[15vw] max-w-[200px] xl:w-[200px] xl:max-w-[200px] z-10"
             />
 
             {/* --- LOGO --- */}
@@ -57,8 +57,8 @@ export default function Hero() {
                 <div className="w-full lg:w-1/2 flex justify-center lg:justify-start pt-24 lg:pt-0">
                     <motion.img
                         src="/img/main.png"
-                        alt="Heroo"
-                        className="w-full max-w-[90%] sm:max-w-[400px] md:max-w-[500px] lg:max-w-[600px] h-auto object-contain"
+                        alt="Hero"
+                        className="w-full max-w-[90%] sm:max-w-[400px] md:max-w-[500px] lg:max-w-[600px] xl:w-[700px] xl:max-w-[700px] h-auto object-contain"
                         initial={{ x: -100, opacity: 0 }}
                         animate={{ x: 0, opacity: 1 }}
                         transition={{ duration: 1.5, ease: 'easeOut' }}
@@ -75,14 +75,22 @@ export default function Hero() {
                     <div className="flex flex-col sm:flex-row items-center lg:flex-col lg:items-start gap-4 max-w-[500px]">
                         {/* Text block */}
                         <div className="text-center lg:text-left">
-                            <h1 className="text-lg sm:text-xl md:text-2xl font-normal">SECURE SOLUTIONS FOR</h1>
-                            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">TOMORROW'S</h1>
-                            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">CHALLENGES</h1>
+                            <h1 className="text-lg sm:text-xl md:text-2xl font-normal">
+                                SECURE SOLUTIONS FOR
+                            </h1>
+                            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">
+                                TOMORROW'S
+                            </h1>
+                            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">
+                                CHALLENGES
+                            </h1>
                         </div>
 
                         {/* "IT" box */}
                         <div className="flex items-center justify-center bg-[#8a9fc4] px-4 py-1 mt-2 sm:mt-0">
-                            <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-black">IT</h1>
+                            <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-black">
+                                IT
+                            </h1>
                         </div>
                     </div>
                 </motion.div>
